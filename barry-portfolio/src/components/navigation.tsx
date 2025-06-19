da8ea0aef821ac16dc466a496ab552f4dbc34889
Barry Molina <bazzaboy@gmail.com>
2025-06-18 22:21:52 -0500

    nav site title

--- barry-portfolio/src/components/navigation.tsx
+++ barry-portfolio/src/components/navigation.tsx
@@ -22,7 +22,7 @@ export function Navigation() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-[var(--nav-height)]">
             <Link href="/" className="text-xl">
-              barryhmolina.com
+              barryhmolina
             </Link>
 
             {/* Desktop Navigation */}
