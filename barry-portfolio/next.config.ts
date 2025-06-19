50a3dd5ea61b651446ce6bcca9b4cf739da40a75
Barry Molina <bazzaboy@gmail.com>
2025-06-18 19:52:16 -0500

    mdx compiles

--- barry-portfolio/next.config.ts
+++ barry-portfolio/next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+export default withMDX(nextConfig);
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
